e6d7fba4ead366fdecfba86764f10565fec78a3d
supun <55055642+supunUOM@users.noreply.github.com>
2024-04-11 17:57:15 +0530

    Update Collection.java

--- Collection.java
+++ Collection.java
@@ -1,3 +1,16 @@
+=== ArrayList ===
+Integer[] arr = {1, 4, 2, 6, 2, 4, 5}; //Array with Wrapper class
+List<Integer> list = Arrays.asList(arr); //create unmodifiable List
+list.sort(Integer::compareTo); //sort the list
+
+List<Integer> list1 = new ArrayList<>(list); // create new List using unmodifiable list
+list1.add(33); // add element
+list1.get(0); //get the 0th element
+list1.remove(0); //remove the 0th element
+list1.isEmpty(); //check list is empty
+list1.contains(3); //check 3 is existing in the list
+list1.containsAll(list2); // if all the elements in list2 present in the list1 it will return true 
+list1.size(); //get the size of the list.
 === HashMap ===
   
 Map<String, Integer> basket = new HashMap<>();
